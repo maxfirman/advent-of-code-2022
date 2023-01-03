88674aea48582cbc570cfd135fb2dc2693441299
maxfirman <max.firman@gmail.com>
2023-01-03 21:56:57 +0000

    AoC 10

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,8 +1,12 @@
-pub fn part_one(input: &str) -> Option<i32> {
+pub fn run(input: &str) -> (i32, String) {
     let mut x = 1_i32;
     let mut i = 1_i32;
     let mut s = 0_i32;
+    let mut string = String::new();
+    println!("{string}");
+
     for line in input.lines() {
+        draw_pixel(&i, &x, &mut string);
         if (i - 20) % 40 == 0 {
             s += i * x;
         }
@@ -11,6 +15,7 @@ pub fn part_one(input: &str) -> Option<i32> {
             i += 1;
         } else if line.starts_with("addx") {
             i += 1;
+            draw_pixel(&i, &x, &mut string);
             if (i - 20) % 40 == 0 {
                 s += i * x;
             }
@@ -18,11 +23,26 @@ pub fn part_one(input: &str) -> Option<i32> {
             i += 1;
         }
     }
-    Some(s)
+    (s, string)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn draw_pixel(i: &i32, x: &i32, string: &mut String) {
+    if (i - 1) % 40 == *x || (i - 1) % 40 == x - 1 || (i - 1) % 40 == x + 1 {
+        string.push('#');
+    } else {
+        string.push('.');
+    }
+    if i % 40 == 0 {
+        string.push('\n');
+    }
+}
+
+pub fn part_one(input: &str) -> Option<i32> {
+    Some(run(input).0)
+}
+
+pub fn part_two(input: &str) -> Option<String> {
+    Some(run(input).1)
 }
 
 fn main() {
@@ -41,9 +61,20 @@ mod tests {
         assert_eq!(part_one(&input), Some(13140));
     }
 
-    // #[test]
-    // fn test_part_two() {
-    //     let input = advent_of_code::read_file("examples", 10);
-    //     assert_eq!(part_two(&input), None);
-    // }
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 10);
+        assert_eq!(
+            part_two(&input),
+            Some(String::from(
+                "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+"
+            ))
+        );
+    }
 }
